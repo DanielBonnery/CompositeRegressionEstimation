2443d75f84ab983f8233ef99b894dbdc0208f667
DanielBonnery <daniel.bonnery@gmail.com>
2016-06-09 11:23:51 -0400

    a

--- R/AK3.R
+++ R/AK3.R
@@ -1,102 +1,49 @@
+#dfest
 AK3 <-
   function(dfest,
            coeff=NULL,
            ak=list(c(a1=0.2,a2=0.3,a3=0,k1=.2,k2=.3,k3=0))){
     #dfest<-WSrg(list.tables,weight="pwsswgt",list.y="pumlr")
     if(is.null(coeff)){
-      coeff<-CoeffAK3(dim(dfest)[1],ak,simplify=FALSE)}
+      coeff<-CoeffAK3CPSl(dim(dfest)[1],ak,simplify=FALSE)}
     asaa=array(apply(dfest,3,function(m1){
       apply(coeff,c(3,4),function(m2){sum(dfest*m2)})}),
       c(dim(dfest)[1],length(ak),dim(dfest)[3]))
-    dmnmsak<-if(is.null(names(ak))){sapply(ak,function(x){paste0("AK2",x[1],"-",x[2])})}else{names(ak)}
+    dmnmsak<-if(is.null(names(ak))){sapply(ak,function(x){paste0("AK3",x[1],"-",x[2])})}else{names(ak)}
     dimnames(asaa)=list(dimnames(dfest)[[1]],dmnmsak,dimnames(dfest)[[3]])
     return(aperm(asaa,c(1,3,2)))}
 
-CoeffAK3<-function(nmonth,ak,simplify=TRUE){
-  if(is.null(names(ak))){names(ak)<-1:length(ak)}
-  coeff<-array(0,c(nmonth,8,nmonth,length(ak),3,3))
-  dimnames(coeff)[4]<-list(sapply(ak,function(x){paste0(x[1],"-",x[2])}))
-  dimnames(coeff)[[5]]<-listpumlrR
-  dimnames(coeff)[[6]]<-listpumlrR
+#ak: a list of vectors of size 6.
+CoeffAK3CPSl<-function(nmonth,ak,simplify=TRUE,statuslabel=c("0","1","_1")){
+  aperm(
+    plyr::laply(ak,function(x){CoeffAK3CPS(nmonth=nmonth,x,simplify=simplify,statuslabel=statuslabel)}),
+    if(simplify){c(2:3,1)}else{c(2:4,1,5:6)})}
+#ak: a  vector of size 6.
+CoeffAK3CPS<-function(nmonth,ak,simplify=TRUE,statuslabel=c("0","1","_1")){
+  coeff<-array(0,c(nmonth,8,nmonth,3,3))
+  dimnames(coeff)[[4]]<-statuslabel
+  dimnames(coeff)[[5]]<-statuslabel
   S<-c(2:4,6:8)
   Sbar<-c(1,5)
   for (u in 1:3){
-    coeff[1,,1,,u,u]<-1
-    for(j in (1:length(ak))){
-      a=ak[[j]][u];k=ak[[j]][3+u];
-      for(i in 2:nmonth){
-        coeff[,,i,j,u,u]<-coeff[,,(i-1),j,u,u]*k
-        coeff[i,,i,j,u,u]<-(1-k)
-        coeff[i-1,S-1,i,j,u,u]<-coeff[i-1,S-1,i,j,u,u]-k*4/3
-        coeff[i,S,i,j,u,u]<-coeff[i,S,i,j,u,u]+k*4/3-8*a/3
-        coeff[i,Sbar,i,j,u,u]<-coeff[i,Sbar,i,j,u,u]+8*a} }}
+    coeff[1,,1,u,u]<-1
+    a=ak[u];k=ak[3+u];
+    for(i in 2:nmonth){
+      coeff[,,i,u,u]<-coeff[,,(i-1),u,u]*k
+      coeff[i,,i,u,u]<-(1-k)
+      coeff[i-1,S-1,i,u,u]<-coeff[i-1,S-1,i,u,u]-k*4/3
+      coeff[i,S,i,u,u]<-coeff[i,S,i,u,u]+k*4/3-8*a/3
+      coeff[i,Sbar,i,u,u]<-coeff[i,Sbar,i,u,u]+8*a} }
   coeff<-coeff/8
   if(simplify){  
-    coeff=array(aperm(coeff,c(5,3,6,2,1,4)),c(nmonth*3,nmonth*8*3,length(ak)))
-    dimnames(coeff)[3]<-list(names(ak))
+    coeff=array(aperm(coeff,c(4,3,5,2,1)),c(nmonth*3,nmonth*8*3))
   }
   return(coeff)}
-
-if(FALSE){
-  
-  y=matrix(rnorm(nmonth*3),nmonth*3,1);
-  coeff<-CoeffAK3(nmonth,list(rnorm(6)))[,,1];mean(coeff%*%X%*%y-y)
-  y=rep(1:0,c(24,231))
-  coeff%*%X%*%y;mean(coeff%*%X%*%y-y)
-  y=rep(1:0,c(24,231))
-  coeff<-CoeffYF(nmonth,Sigma);mean(coeff%*%X%*%y-y)
-  a1=.1;a2=.2;k1=.1;k2=.2
-  ak=list(c(a1=a1,a2=a1,a3=a1,k1=k1,k2=k1,k3=k1),c(a1=a2,a2=a2,a3=a2,k1=k2,k2=k2,k3=k2))
-  coeffAK3<-CoeffAK3(nmonth,ak=ak)
-  popnum<-1
-  adde2<-"_1"
-  nrepe=1
-  #charge(paste0("coeffAK3_",popnum))    
-  charge(paste0("mistotalscomp",adde2))
-  mtc<-array(aperm(mistotalscomp[,,,1:nrepe,drop=FALSE],c(3,2,1,4)),c(prod(dim(mistotalscomp)[1:3]),nrepe))
-  AK3comprep<-aperm(array(apply(mtc,2,function(X){apply(coeffAK3,3,function(m2){m2%*%X})}),c(3,nmonth,dim(coeffAK3)[3],nrep)),c(2,1,3,4))
-  dimnames(AK3comprep)<-c(dimnames(mistotalscomp[,,,1:nrepe])[c(1,3)],list(paste0("AK3",dimnames(coeffAK3)[[3]])),list(1:nrepe))
-  AK3comprep<-addU(AK3comprep)
-  
-  
-  ak2<-list(c(.1,.1),c(.2,.2))
-  coeff=CoeffAK3(nmonth,ak=ak)
-  dfest=mistotalscomp[,,,1:nrep]
-  AAKK2<-       AK2(dfest, coeff=coeff,ak=ak2)
-  any(replicate(1000,
-                (function(){x<-sample(85,1);y<-sample(3,1);z<-sample(2,1);
-                            abs(AAKK2[x,y,z]-AK3comprep[x,y,z,1])>1e-8})()))
-  
-  charge("Sigma1")
-  ak3=c(a1=a1,a2=a1,a3=a1,k1=k1,k2=k1,k3=k1)
-  coeffAK3<-CoeffAK3(nmonth,list(ak3),simplify=TRUE)
-  coeffAK2<-CoeffAK2(nmonth,list(ak2[[1]]))
-  
-  x<-sapply(c(85,3,85,8),function(x){sample(x,1)});x;
-  abs(coeffAK3[(x[1]-1*3)+x[2],(x[3]-1)*8*3+(x[4]-1)*3+x[2],]-
-        coeffAK2[x[3],x[4],x[1],])>1e-3;
-  
-  coeffAK3<-CoeffAK3(nmonth,list(ak3),simplify=FALSE)
-  x<-sapply(dim(coeffAK3),function(x){sample(x,1)});x;
-  any(replicate(10000,abs(coeffAK3[x[1],x[2],x[3],x[4],x[5],x[6]]-
-                            coeffAK2[x[1],x[2],x[3],x[4]])>1e-3));
-  
-  
-  varak2<-varAK(ak2[[1]],Sigma)
-  varak3<-varAK3(ak3,Sigma)
-  any(replicate(10000,
-                (function(){x<-sapply(rep(c(85,3),each=2),function(x){sample(x,1)})
-                            abs(varak2[x[1],x[2],x[3],x[4]]-varak3[x[1],x[2],x[3],x[4]])>1e-8})()))
-  
-  varak3[1,1,1,1]
-  varak2[1,1,1,1]
-  
-}
 
 
 
 CoeffAK3diff<-function(nmonth,ak,simplify=TRUE){
-  coeff<-CoeffAK3(nmonth,ak,simplify=FALSE)
+  coeff<-CoeffAK3CPSl(nmonth,ak,simplify=FALSE)
   coeff[,,2:nmonth,,,]<-coeff[,,2:nmonth,,,,drop=FALSE]-coeff[,,1:(nmonth-1),,,,drop=FALSE]
   coeff[,,1,,,]<-0
   if(simplify){
@@ -109,7 +56,7 @@ CoeffAK3diff<-function(nmonth,ak,simplify=TRUE){
 
 varAK3<-function(ak,Sigma){
   nmonth<-dim(Sigma)[1]
-  coeff<-CoeffAK3(nmonth,list(ak))[,,1]
+  coeff<-CoeffAK3CPSl(nmonth,list(ak))[,,1]
   XX=array(coeff%*%(array(aperm(Sigma,c(3:1,6:4)),rep(c(nmonth*8*3),2)))%*%t(coeff),rep(c(3,nmonth),2))
   dimnames(XX)<-rep(dimnames(Sigma)[c(3,1)],2)
   aperm(XX,c(2,4,1,3))}
@@ -122,12 +69,11 @@ varAK3diff<-function(ak,Sigma){
   dimnames(XX)<-rep(dimnames(Sigma)[c(3,1)],2)
   aperm(XX,c(2,4,1,3))}
 
-varAK3rat<-function(ak,Sigma,Scomppop){
+varAK3rat<-function(ak,Sigma,Scomppop,what=c(unemployed="0",employed="1")){
   nmonth<-dim(Sigma)[1]
-  what=c(paste0("pumlrR_n",0:1))
   mati<-matrix(c(1,1,0,1),2,2)
   SM<-Scomppop[,what]
-  X<-SM[,"pumlrR_n0"];Y<-apply(SM,1,sum)
+  X<-SM[,what["unemployed"]];Y<-apply(SM,1,sum)
   V<-rbind(1,-X/Y)
   sigma<-varAK3(ak,Sigma)
   VV<-sapply(1:nmonth,function(i){V[,i]%*%mati%*%sigma[i,i,what,what]%*%t(V[,i]%*%mati)})/(Y^2)
@@ -165,27 +111,6 @@ varAK3_n0mean2comp    <-function(ak,Sigma){varAK3_n0mean(ak2_n0to6(ak),Sigma)+va
 
 
 #
-if (FALSE){
-  ak<-c(.2,.3,0,.2,.3,0)
-  Sigma2<-varAK3(ak,Sigma)
-  ccma<-calcvarmeana(AKcomprep)
-  par(mfrow=c(1,3))
-  sapply(listpumlrR,function(ccc){plot(ccma[,ccc,,"var"],sapply(1:85,function(i){Sigma2[i,i,ccc,ccc]}))})
-  Sigma3<-varAK3rat(ak,Sigma,Scomppop)
-  par(mfrow=c(1,1))
-  ccc<-"unemployment";plot(ccma[,ccc,,"var"],Sigma3)
-  
-  Sigma2<-varAKdiff3(ak,Sigma)
-  ccma<-calcvarmeanadiff(AK2_papacomprep,ScomppopUdiff)
-  par(mfrow=c(1,3))
-  sapply(listpumlrR,function(ccc){plot(ccma[,ccc,,"var"],sapply(1:84,function(i){Sigma2[i,i,ccc,ccc]}))})
-  Sigma3<-varAKdiffrat(ak,Sigma,Scomppop)
-  par(mfrow=c(1,1))
-  ccc<-"unemployment";plot(ccma[,ccc,,"var"],Sigma3)
-  
-  
-  
-}
 ak2_n0to6<-function(ak){c(ak[1],0,0,ak[2],0,0)}
 ak2_n1to6<-function(ak){c(0,ak[1],0,0,ak[2],0)}
 
@@ -209,14 +134,14 @@ bestAK3<-function(Sigma,Scomppop,
                   what=list(level=varAK3ratmean4,change=varAK3diffratmean4,compromise=varAK3compratmean4)){
   lapply(what,function(fn){
     oo<-optimx::optimx(startval,
-               fn=fn,itnmax=itnmax,method="Nelder-Mead",
-               Sigma=Sigma,Scomppop=Scomppop);
+                       fn=fn,itnmax=itnmax,method="Nelder-Mead",
+                       Sigma=Sigma,Scomppop=Scomppop);
     ak4to6(c(oo$p1,oo$p2,oo$p3,oo$p4))})}
 
 
 bestAK3grille<-function(Sigma,Scomppop,
-                  itnmax=100,
-                  what=list(level=varAK3ratmean4,change=varAK3diffratmean4,compromise=varAK3compratmean4)){
+                        itnmax=100,
+                        what=list(level=varAK3ratmean4,change=varAK3diffratmean4,compromise=varAK3compratmean4)){
   n=4
   X=seq(0,1,length=10^n+1)
   ak3all<-lapply(X,function(x){y<-floor(x*10^(1:n));y<-(y-10*c(0,y[1:(n-1)]))/10;c(y[1:2],0,y[3:4],1,0)})
@@ -240,10 +165,10 @@ bestAK3sep<-function(Sigma,
   oo0c<-optimx(startval[c(1,3)],
                fn=varAK3_n0mean2comp,itnmax=itnmax,method="Nelder-Mead",
                Sigma=Sigma);
-list(levelsep     =c(oo0$p1 ,oo1$p1,0,oo0$p2 ,oo1$p2,0),
-     changesep    =c(oo0d$p1,oo1$p1,0,oo0d$p2,oo1$p2,0),
-     compromisesep=c(oo0c$p1,oo1$p1,0,oo0c$p2,oo1$p2,0))
-
+  list(levelsep     =c(oo0$p1 ,oo1$p1,0,oo0$p2 ,oo1$p2,0),
+       changesep    =c(oo0d$p1,oo1$p1,0,oo0d$p2,oo1$p2,0),
+       compromisesep=c(oo0c$p1,oo1$p1,0,oo0c$p2,oo1$p2,0))
+  
 }
 
 
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Working document 
-
 `CompositeRegressionEstimation` is an R package that allows to compute estimators for longitudinal survey:
-* Composite Regression ["Fuller, Wayne A., and J. N. K. Rao. "A regression composite estimator with application to the Canadian Labour Force Survey." Survey Methodology 27.1 (2001): 45-52."](http://www.statcan.gc.ca/pub/12-001-x/2001001/article/5853-eng.pdf)estimator contains the source code to reproduce the simulations of [""]() by Bonnery, Lahiri and Tran
+* Composite Regression ["Fuller, Wayne A., and J. N. K. Rao. "A regression composite estimator with application to the Canadian Labour Force Survey." Survey Methodology 27.1 (2001): 45-52."](http://www.statcan.gc.ca/pub/12-001-x/2001001/article/5853-eng.pdf)
 * Yansaneh Fuller
 * AK estimator
 * 
+
 ## Install package
 
 ```r
@@ -12,70 +12,11 @@ devtools::install_github("DanielBonnery/CompositeRegressionEstimation")
 ```
 
 
-## R code
+## Demonstration code
 
 ```r
 library(dataASPEP)
 library(pubBonneryLahiriTran2016)
 library(ggplot2)
 ```
-
-#1. Extract data: 2007 and 2012 census corresponding to 2011 sample.
-
-
-```
-FALSE ```r
-FALSE function () 
-FALSE {
-FALSE     data(aspep2007, aspep2011, aspep2012, aspep2007_gov)
-FALSE     sqldf::sqldf("\nselect a.id,\n  d.type_of_gov,\n  b.itemcode,\n  d.state,\n  log10(1+c.ftpay) as lftpay, \n  log10(1+a.ftpay) as lftpay07 \nfrom \n   aspep2007 a, \n   aspep2011 b,\n   aspep2012 c,\n   aspep2007_gov d\n   where a.id=b.id \n      and a.id=c.id\n      and a.id=d.id \n      and a.itemcode=b.itemcode  \n      and a.itemcode=c.itemcode")
-FALSE }
-FALSE <environment: namespace:pubBonneryLahiriTran2016>
-FALSE ```
-```
-
-##1.1. Plot 1
-
-```r
-demo(plot1)
-```
-
-
-```r
-print(plot1)
-```
-
-![plot of chunk r5](figure/r5-1.png)
-
-#2. Models
-
-##2.1. Simple model
-
-$$\mathrm{ftpay}_{2012,k}=\beta_{0,\mathrm{state}_k,\mathrm{code}_k,\mathrm{type}_k}+
-\beta_{0,\mathrm{state}_k,\mathrm{code}_k,\mathrm{type}_k}\times \mathrm{ftpay}_{2007,k}+\varepsilon_k$$
-
-
-```r
-demo(model1)
-```
-
-
-
-
-
-```r
-print(plot1)
-```
-
-![plot of chunk unnamed-chunk-2](figure/unnamed-chunk-2-1.png)
-
-
-
-
-
-
-
-
-
-
 
