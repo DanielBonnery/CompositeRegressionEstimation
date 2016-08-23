8e1527df361ccdd9c789ee0e1fc2bb95280dbd15
DanielBonnery <daniel.bonnery@gmail.com>
2016-08-22 23:29:46 -0400

    a

--- R/factorisedf.R
+++ R/factorisedf.R
@@ -1,25 +1,25 @@
-factorisedf <-function(df,list.y){
-  if(!is.null(list.y)){
-    toconvert <- list.y[sapply(as.data.frame(df[, list.y,drop = FALSE]), class) %in% (c("factor", "character"))]
-    toconvert_n <- toconvert[sapply(as.data.frame(df[, toconvert, drop = FALSE]), function(l) {length(unique(l))}) > 1]
+factorisedf <-function(dfr,list.y){
+
+    toconvert <- list.y[sapply(as.data.frame(dfr[, list.y,drop = FALSE]), class) %in% (c("factor", "character"))]
+    toconvert_n <- toconvert[sapply(as.data.frame(dfr[, toconvert, drop = FALSE]), function(l) {length(unique(l))}) > 1]
     toconvert_1 <- setdiff(toconvert, toconvert_n)
     numericv<-setdiff(list.y,toconvert)
   
-    fdf=df[,numericv,drop=FALSE]
+    fdf=dfr[,numericv,drop=FALSE]
     
     for(varn in toconvert_n){
-      df[,varn]<-paste("_n",factor(df[,varn],exclude=NULL),sep="")
+      dfr[,varn]<-paste("_n",factor(dfr[,varn],exclude=NULL),sep="")
       formulaa=as.formula(paste(c(" ~ 0  ", varn), collapse=" + "))
-      fdf=cbind(fdf,model.matrix(formulaa, df))}
+      fdf=cbind(fdf,model.matrix(formulaa, dfr))}
     for(varn in toconvert_1){
-      if(is.factor(df[,varn])){
-        leve<-levels(df[,varn])
-        uni<-leveunique(df[,varn])
+      if(is.factor(dfr[,varn])){
+        leve<-levels(dfr[,varn])
+        uni<-leveunique(dfr[,varn])
         fdf[,paste0(varn,"_n",leve)]<-0
-        fdf[,paste0(varn,"_n",leve[unique(df[,varn])])]<-1
-      }else{fdf[,paste0(varn,"_n",unique(df[,varn]))]<-1}}
+        fdf[,paste0(varn,"_n",leve[unique(dfr[,varn])])]<-1
+      }else{fdf[,paste0(varn,"_n",unique(dfr[,varn]))]<-1}}
     fdf<-fdf[,setdiff(names(fdf),names(fdf)[grep("_nNA",names(fdf))]),drop=FALSE]
-    names(fdf)<-gsub("-","_",names(fdf))}else{fdf=df[,character(0),drop=FALSE]}
+    names(fdf)<-gsub("-","_",names(fdf))
   
   return(list(fdf=fdf,#all columns from df + fdf
               fdf2=fdf,#same
