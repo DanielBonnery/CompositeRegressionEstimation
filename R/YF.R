fd49291d42bb670a44780f723b2c439bf7ff5bc1
DanielBonnery <daniel.bonnery@gmail.com>
2016-09-05 16:13:53 -0400

    a

--- R/YF.R
+++ R/YF.R
@@ -1,6 +1,8 @@
 #Sigma: an array  
 
-CoeffYF<-function(Sigma,nmonth=dim(Sigma)[[2]]){
+CPS_X_matrix<-function(){}
+
+CoeffYF<-function(Sigma,nmonth=dim(Sigma)[[1]]){
   Sigg<-array(aperm(Sigma,c(3,2,1,6,5,4)),rep(nmonth*8*3,2))
   XX<-t(matrix(rep(diag(3),8),3,24))
   X<-as.matrix(Matrix::.bdiag(lapply(1:nmonth,function(i){XX})))
@@ -9,6 +11,25 @@ CoeffYF<-function(Sigma,nmonth=dim(Sigma)[[2]]){
   gi<-MASS::ginv(M%*%Sigg%*%M)
   W<-Xplus%*%(diag(nmonth*8*3)-M)%*%(diag(nmonth*8*3)-Sigg%*%gi)
   return(W)}
+
+#' Compute the coefficients for Multivariate Blue
+#' 
+#' @param Sigma a p x p matrix
+#' @param X an n x p  matrix
+#' @param Xplus: the general inverse of X
+#' @return the coefficients matrix $W$ such that $WY$ is the best unbiased linear estimator of $\beta$ where $E[Y]=X\beta$
+#' @examples
+#' A=array(rnorm(prod(2:5)),2:5);M=a2m(A,2);dim(A);dim(M);dim(a2m(A))
+
+CoeffYF.matrix<-function(Sigma,X,Xplus=MASS::ginv(X)){
+  M<-diag(nrow(X))-X%*%Xplus
+  Xplus%*%(diag(nrow(X))-M)%*%(diag(nrow(X))-Sigma%*%(MASS::ginv(M%*%Sigma%*%M)))}
+
+
+CoeffYF.tensor<-function(Sigma,X,Xplus=MASS::ginv(X)){
+  CoeffYF.matrix()}
+
+
 
 CoeffS2<-function(nmonth){
   XX<-t(matrix(rep(diag(3),8),3,24))
